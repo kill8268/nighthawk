f52b4d0c1bb6c98ca5bb8322057b69ace3498708
kill8268 <kill8268@main-serve.kill8268-serve.one>
2023-08-01 00:02:55 +0800

    themoviedb api

--- app/page.js
+++ app/page.js
@@ -1,7 +1,36 @@
-export default function Home() {
+import Image from 'next/image'
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+import themoviedb from "@/lib/themoviedb"
+
+export default async function Home() {
+
+  const { results: movie } = await themoviedb('/discover/movie')
+
+  console.info(movie)
+  
   return (
     <main className="flex min-h-screen">
-      hi
+      <div className="w-[200px] border border-t-0 border-b-0 border-l p-2">
+        <div className="text-2xl font-semibold">hi</div>
+      </div>
+      <div className="flex-1 p-2 grid grid-cols-5 gap-4">
+        {movie.map(item => (
+          <div key={item.id}>
+            <AspectRatio ratio={1 / 1.6} className="bg-muted">
+              <Image
+                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                alt="backdrop_path"
+                fill
+                className="rounded-md object-cover"
+              />
+            </AspectRatio>
+            <div className="text-2xl font-semibold">{item.title}</div>
+            {/* <div className="text-sm">
+              {item.overview}
+            </div> */}
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['tmdb.org', 'image.tmdb.org', 'themoviedb.org'],
+  },
+}
 
 module.exports = nextConfig
